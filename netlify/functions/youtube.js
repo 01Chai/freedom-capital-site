fec4bfc34f66416642f2496ee0b23e77d184f39a
01Chai <123114872+01Chai@users.noreply.github.com>
2025-10-26 07:15:35 -0400

    Update youtube.js

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,28 +1,19 @@
-// netlify/functions/youtube.js
 const fetch = require("node-fetch");
 
-exports.handler = async function () {
+exports.handler = async function (event, context) {
+  const API_KEY = http://process.env.YOUTUBE_API_KEY;
+  const CHANNEL_ID = "UCjpzeWEU0-629Baz_RA-LbQ";
+  const API_URL = `https://googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=3`;
+
   try {
-    const apiKey = http://process.env.YOUTUBE_API_KEY;
-    const channelId = "UCjpzeWEU0-629Baz_RA-LbQ";
-    const maxResults = 4; // change to how many videos you want
-
-    const url = `https://googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}`;
-
-    const response = await fetch(url);
+    const response = await fetch(API_URL);
     const data = await response.json();
 
-    if (!response.ok) {
-      throw new Error(`YouTube API error: ${response.statusText}`);
-    }
-
-    const videos = data.items
-      .filter(item => http://item.id.kind === "youtube#video")
-      .map(item => ({
-        title: item.snippet.title,
-        videoId: http://item.id.videoId,
-        thumbnail: item.snippet.thumbnails.medium.url,
-      }));
+    const videos = http://data.items.map((item) => ({
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.medium.url,
+      videoId: http://item.id.videoId,
+    }));
 
     return {
       statusCode: 200,
