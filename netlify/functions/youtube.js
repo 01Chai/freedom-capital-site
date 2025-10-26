68c2941ce22e5b81a55692acf2117c5b0aecab11
01Chai <123114872+01Chai@users.noreply.github.com>
2025-10-26 07:34:28 -0400

    Fix API key retrieval and update API URL

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,18 +1,18 @@
 const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
-  const API_KEY = http://process.env.YOUTUBE_API_KEY;
+  const API_KEY = process.env.YOUTUBE_API_KEY;
   const CHANNEL_ID = "UCjpzeWEU0-629Baz_RA-LbQ";
-  const API_URL = `https://googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=3`;
+  const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=3`;
 
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
 
-    const videos = http://data.items.map((item) => ({
+    const videos = data.items.map((item) => ({
       title: item.snippet.title,
       thumbnail: item.snippet.thumbnails.medium.url,
-      videoId: http://item.id.videoId,
+      videoId: item.id.videoId,
     }));
 
     return {
